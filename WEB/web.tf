b690ffd31af8344a6265a23b6eb6e75e6adfd9a6
leye alex-oni <leyealexoni@gmail.com>
2017-09-17 14:27:01 +0100

    Alex Oni: Initial Web Configuration file added

--- WEB/web.tf
+++ WEB/web.tf
@@ -1,1 +1,40 @@
 #Terraform Configuration file for Web Apps
+
+#Provider details
+ provider "aws" { 
+     region = "eu-west-2"
+   }
+
+#Web Instance Configuration
+ resource "aws_instance" "app1" { 
+   ami  = "ami-996372fd"
+   instance_type = "t2.micro"
+   vpc_security_group_ids = ["${aws_security_group.websecgrp.id}"]
+
+ tags {
+   Name = "app1-svr"
+  }
+}
+
+#Simple Web Architecture Configuration - Bash Script
+ user_data = <<-EOF
+             #!/bin/bash
+             echo "Hello, World" > index.html
+             nohup busybox httpd -f -p 8080 &
+             EOF
+
+#Creates Security Group for EC2 web server traffic
+ resource "aws_security_group" "websecgrp" {
+    name = "websecgrp-SG"
+    
+    ingress {
+       from_port  = 8080
+       to_port    = 8080
+       protocol   = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+     }
+ }
+
+
+
+
