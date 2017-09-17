3788c1fe773b0f102bc75215a68d648e502a3c55
leye alex-oni <leyealexoni@gmail.com>
2017-09-17 16:57:23 +0100

    Alex Oni: Config files for DRY config committed

---
+++ WEB-DRY/variables.tf
@@ -0,0 +1,15 @@
+#Contains the variables used for this Terraform configuration
+
+ variable "region" {
+     description = "Uses the London Region"
+   }
+
+  variable "ami" {
+      description = "AWS Machine Image used for instance"
+   }
+
+  variable "server_port" {
+      description = "The port the server will use for HTTP requests"
+
+  
+ 
---
+++ WEB-DRY/web-dry.tf
@@ -0,0 +1,3 @@
+/*Terraform Configuration file for Simple Web Server publishing Hello,World Web Page on port 8080. This time Parameterizing the config file with the use of variables */
+
+
