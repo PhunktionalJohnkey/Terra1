1965fc8a7918c2c683322a4eb28317fd44de9b4d
leye alex-oni <leyealexoni@gmail.com>
2017-09-17 15:23:03 +0100

    Alex Oni: web.tf file modified to include further documentation aids in the form of comments.

--- WEB/web.tf
+++ WEB/web.tf
@@ -1,4 +1,4 @@
-#Terraform Configuration file for Web Apps
+/*Terraform Configuration file for Simple Web Server publishing  Hello, World Web Page on port 8080 */
 
 #Provider details
  provider "aws" { 
@@ -11,17 +11,17 @@
    instance_type = "t2.micro"
    vpc_security_group_ids = ["${aws_security_group.websecgrp.id}"]
 
- tags {
-   Name = "app1-svr"
-  }
-}
-
 #Simple Web Architecture Configuration - Bash Script
  user_data = <<-EOF
              #!/bin/bash
              echo "Hello, World" > index.html
              nohup busybox httpd -f -p 8080 &
              EOF
+
+ tags {
+   Name = "app1-svr"
+  }
+}
 
 #Creates Security Group for EC2 web server traffic
  resource "aws_security_group" "websecgrp" {
